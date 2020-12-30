8d1f2b512d212a6bb69d1f0024ad9118c0eb1b92
Matt Kiazyk <mattkiazyk@gmail.com>
2020-12-30 15:43:33 -0600

    Fixes acknowledgment screen in darkmode

--- Xcodes/Frontend/About/AcknowledgementsView.swift
+++ Xcodes/Frontend/About/AcknowledgementsView.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 struct AcknowledgmentsView: View {
+    
     var body: some View {
         ScrollingTextView(
             attributedString: NSAttributedString(
                 rtf: try! Data(contentsOf: Bundle.main.url(forResource: "Licenses", withExtension: "rtf")!), 
                 documentAttributes: nil
             )!
+            .addingAttribute(.foregroundColor, value: NSColor.labelColor)
         )
         .frame(minWidth: 500, minHeight: 500)
     }
