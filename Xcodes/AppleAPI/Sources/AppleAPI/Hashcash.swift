76bb3fbae0f945673198818116b3d61b8349999a
Matt Kiazyk <mattkiazyk@gmail.com>
2023-02-27 12:11:12 -0600

    Adds hashcash implementation

--- Xcodes/AppleAPI/Sources/AppleAPI/Hashcash.swift
+++ Xcodes/AppleAPI/Sources/AppleAPI/Hashcash.swift
@@ -9,13 +9,44 @@ import Foundation
 import CryptoKit
 import CommonCrypto
 
+/*
+# This App Store Connect hashcash spec was generously donated by...
+ #
+ #                         __  _
+ #    __ _  _ __   _ __   / _|(_)  __ _  _   _  _ __  ___  ___
+ #   / _` || '_ \ | '_ \ | |_ | | / _` || | | || '__|/ _ \/ __|
+ #  | (_| || |_) || |_) ||  _|| || (_| || |_| || |  |  __/\__ \
+ #   \__,_|| .__/ | .__/ |_|  |_| \__, | \__,_||_|   \___||___/
+ #         |_|    |_|             |___/
+ #
+ #
+*/
 public struct Hashcash {
-
+    /// A function to returned a minted hash, using a bit and resource string
+    ///
+    /**
+      X-APPLE-HC: 1:11:20230223170600:4d74fb15eb23f465f1f6fcbf534e5877::6373
+                  ^  ^      ^                       ^                     ^
+                  |  |      |                       |                     +-- Counter
+                  |  |      |                       +-- Resource
+                  |  |      +-- Date YYMMDD[hhmm[ss]]
+                  |  +-- Bits (number of leading zeros)
+                  +-- Version
+     
+     We can't use an off-the-shelf Hashcash because Apple's implementation is not quite the same as the spec/convention.
+     1. The spec calls for a nonce called "Rand" to be inserted between the Ext and Counter. They don't do that at all.
+     2. The Counter conventionally encoded as base-64 but Apple just uses the decimal number's string representation.
+      
+     Iterate from Counter=0 to Counter=N finding an N that makes the SHA1(X-APPLE-HC) lead with Bits leading zero bits
+     We get the "Resource" from the X-Apple-HC-Challenge header and Bits from X-Apple-HC-Bits
+     */
+    /// - Parameters:
+    ///    - resource: a string to be used for minting
+    ///    - bits: grabbed from `X-Apple-HC-Bits` header
+    ///    - date: Default uses Date() otherwise used for testing to check.
+    /// - Returns: A String hash to use in `X-Apple-HC` header on /signin
     public func mint(resource: String,
-                     bits: UInt = 20,
-                     ext: String = "",
-                     saltCharacters: UInt = 16,
-                     stampSeconds: Bool = true,
+                     bits: UInt = 10,
                      date: String? = nil) -> String? {
         
         let ver = "1"
@@ -25,15 +56,13 @@ public struct Hashcash {
             ts = date
         } else {
             let formatter = DateFormatter()
-            formatter.dateFormat = stampSeconds ? "yyMMddHHmmss" : "yyMMdd"
+            formatter.dateFormat = "yyMMddHHmmss"
             ts = formatter.string(from: Date())
         }
         
         let challenge = "\(ver):\(bits):\(ts):\(resource):"
         
         var counter = 0
-        let hexDigits = Int(ceil((Double(bits) / 4)))
-        let zeros = String(repeating: "0", count: hexDigits)
         
         while true {
             guard let digest = ("\(challenge):\(counter)").sha1 else {
@@ -41,87 +70,27 @@ public struct Hashcash {
                 return nil
             }
             
-            if digest.prefix(hexDigits) == zeros {
+            if digest == bits {
                 return "\(challenge):\(counter)"
             }
             counter += 1
         }
     }
-    
-    /**
-     Checks whether a stamp is valid
-     - parameter stamp: stamp to check e.g. 1:16:040922:foo::+ArSrtKd:164b3
-     - parameter resource: resource to check against
-     - parameter bits: minimum bit value to check
-     - parameter expiration: number of seconds old the stamp may be
-     - returns: true if stamp is valid
-     */
-    public func check(stamp: String,
-                      resource: String? = nil,
-                      bits: UInt,
-                      expiration: UInt? = nil) -> Bool {
-        
-        guard let stamped = Stamp(stamp: stamp) else {
-            print("Invalid stamp format")
-            return false
-        }
-        
-        if let res = resource, res != stamped.resource {
-            print("Resources do not match")
-            return false
-        }
-        
-        var count = bits
-        if let claim = stamped.claim {
-            if bits > claim {
-                return false
-            } else {
-                count = claim
-            }
-        }
-        
-        if let expiration = expiration {
-            let goodUntilDate = Date(timeIntervalSinceNow: -TimeInterval(expiration))
-            if (stamped.date < goodUntilDate) {
-                print("Stamp expired")
-                return false
-            }
-        }
-        
-        guard let digest = stamp.sha1 else {
-            return false
-        }
-        
-        let hexDigits = Int(ceil((Double(count) / 4)))
-        return digest.hasPrefix(String(repeating: "0", count: hexDigits))
-    }
-    
-    /**
-     Generates random string of chosen length
-     - parameter length:    length of random string
-     - returns: random string
-     */
-    internal func salt(length: UInt) -> String {
-        let allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/="
-        var result = ""
-        
-        for _ in 0..<length {
-            let randomValue = arc4random_uniform(UInt32(allowedCharacters.count))
-            result += "\(allowedCharacters[allowedCharacters.index(allowedCharacters.startIndex, offsetBy: Int(randomValue))])"
-        }
-        return result
-    }
 }
 
 extension String {
-    var sha1: String? {
+    var sha1: Int? {
+        
         let data = Data(self.utf8)
         var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
         data.withUnsafeBytes {
             _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &digest)
         }
-        let hexBytes = digest.map { String(format: "%02x", $0) }
-        return hexBytes.joined()
+        let bigEndianValue = digest.withUnsafeBufferPointer {
+                 ($0.baseAddress!.withMemoryRebound(to: UInt32.self, capacity: 1) { $0 })
+        }.pointee
+        let value = UInt32(bigEndian: bigEndianValue)
+        return value.leadingZeroBitCount
     }
 }
 
--- Xcodes/AppleAPI/Tests/AppleAPITests/AppleAPITests.swift
+++ Xcodes/AppleAPI/Tests/AppleAPITests/AppleAPITests.swift
@@ -4,21 +4,24 @@ import XCTest
 final class AppleAPITests: XCTestCase {
     
     func testValidHashCashMint() {
+        let bits: UInt = 11
+        let resource = "4d74fb15eb23f465f1f6fcbf534e5877"
+        let testDate = "20230223170600"
+ 
+        let stamp = Hashcash().mint(resource: resource, bits: bits, date: testDate)
+        XCTAssertEqual(stamp, "1:11:20230223170600:4d74fb15eb23f465f1f6fcbf534e5877::6373")
+    }
+    func testValidHashCashMint2() {
         let bits: UInt = 10
         let resource = "bb63edf88d2f9c39f23eb4d6f0281158"
         let testDate = "20230224001754"
-        
-//    "1:11:20230224004345:8982e236688f6ebf588c4bd4b445c4cc::877"
-//        7395f792caf430dca2d07ae7be0c63fa
-        
+ 
         let stamp = Hashcash().mint(resource: resource, bits: bits, date: testDate)
-        XCTAssertNotNil(stamp)
         XCTAssertEqual(stamp, "1:10:20230224001754:bb63edf88d2f9c39f23eb4d6f0281158::866")
-        
-        print(stamp)
     }
 
     static var allTests = [
         ("testValidHashCashMint", testValidHashCashMint),
+        ("testValidHashCashMint2", testValidHashCashMint2),
     ]
 }
