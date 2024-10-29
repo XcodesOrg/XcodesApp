42c2c6bfc059f44c7add27e3ac425ff3278d1ddd
Matt Kiazyk <mattkiazyk@gmail.com>
2024-10-28 21:45:07 -0500

    move to xcodesOrg/swift-srp fork

--- Xcodes/AppleAPI/Package.swift
+++ Xcodes/AppleAPI/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["AppleAPI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/abiligiri/swift-srp", from: "1.1.0")
+        .package(url: "https://github.com/xcodesOrg/swift-srp", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
